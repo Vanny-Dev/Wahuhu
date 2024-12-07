140d2d4fd750350068211895b78fe43535189874
Jovanny De Leon <roanlandao05@gmail.com>
2024-12-07 17:36:12 +0800

    remove vcnotif temp

--- features/vcnotif.js
+++ features/vcnotif.js
@@ -1,53 +1,53 @@
-const { Client, GatewayIntentBits, IntentsBitField, Partials } = require("discord.js")
+// const { Client, GatewayIntentBits, IntentsBitField, Partials } = require("discord.js")
 
-const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-		    GatewayIntentBits.GuildVoiceStates,
-        IntentsBitField.Flags.GuildMembers,
-        IntentsBitField.Flags.GuildMessages,
-        IntentsBitField.Flags.MessageContent,
-		    GatewayIntentBits.GuildPresences
-    ],
-    partials: [Partials.Message, Partials.Channel, Partials.Reaction],
-});
+// const client = new Client({
+//     intents: [
+//         GatewayIntentBits.Guilds,
+// 		    GatewayIntentBits.GuildVoiceStates,
+//         IntentsBitField.Flags.GuildMembers,
+//         IntentsBitField.Flags.GuildMessages,
+//         IntentsBitField.Flags.MessageContent,
+// 		    GatewayIntentBits.GuildPresences
+//     ],
+//     partials: [Partials.Message, Partials.Channel, Partials.Reaction],
+// });
 
 
-client.on('voiceStateUpdate', async (oldState, newState) => {
+// client.on('voiceStateUpdate', async (oldState, newState) => {
    
-    if (newState.channel && !oldState.channel) {
+//     if (newState.channel && !oldState.channel) {
         
-        try {
-            const channel = newState.channel;
-            await channel.send(`<@${newState.member.id}> joined the vc.`);
-        } catch (err) {
-            console.error('Error sending message when joining VC:', err);
-        }
-    }
+//         try {
+//             const channel = newState.channel;
+//             await channel.send(`<@${newState.member.id}> joined the vc.`);
+//         } catch (err) {
+//             console.error('Error sending message when joining VC:', err);
+//         }
+//     }
 
     
-    if (!newState.channel && oldState.channel) {
+//     if (!newState.channel && oldState.channel) {
       
-        try {
-            const channel = oldState.channel;
-            await channel.send(`<@${oldState.member.id}> left the vc.`);
-        } catch (err) {
-            console.error('Error sending message when leaving VC:', err);
-        }
-    }
+//         try {
+//             const channel = oldState.channel;
+//             await channel.send(`<@${oldState.member.id}> left the vc.`);
+//         } catch (err) {
+//             console.error('Error sending message when leaving VC:', err);
+//         }
+//     }
 
     
-    if (newState.channel && oldState.channel && newState.channel.id !== oldState.channel.id) {
-        try {
-            const oldChannel = oldState.channel;
-            const newChannel = newState.channel;
+//     if (newState.channel && oldState.channel && newState.channel.id !== oldState.channel.id) {
+//         try {
+//             const oldChannel = oldState.channel;
+//             const newChannel = newState.channel;
             
-            await oldChannel.send(`<@${newState.member.id}> switched to another vc.`);
-            await newChannel.send(`<@${newState.member.id}> joined the vc.`);
-        } catch (err) {
-            console.error('Error sending message when switching VC:', err);
-        }
-    }
-});
+//             await oldChannel.send(`<@${newState.member.id}> switched to another vc.`);
+//             await newChannel.send(`<@${newState.member.id}> joined the vc.`);
+//         } catch (err) {
+//             console.error('Error sending message when switching VC:', err);
+//         }
+//     }
+// });
 
-client.login(process.env.TOKEN);
+// client.login(process.env.TOKEN);
